98b1b5caf417eb9510159a17f9c97f5742df7862
Eric Griffith <griffith.eric@pm.me>
2026-02-01 15:48:28 -0600

    refactor: move main logic and interfaces from `bin/` to `lib/`

--- bin/main.ml
+++ bin/main.ml
@@ -1,27 +1,9 @@
 open Cmdliner
-
-let fibonacci n =
-  let rec inner a b count =
-    match count with
-    | _ when Z.(count = zero) -> Ok a
-    | _ -> inner b (Z.add a b) (Z.sub count Z.one)
-  in
-  inner Z.zero Z.one (Z.of_int n)
-;;
+open Fibocaml
 
 let number_arg =
   let doc = "The Fibonacci number to generate. Defaults to 0" in
   Arg.(value & pos 0 int 0 & info [] ~docv:"NUMBER" ~doc)
-;;
-
-let run number =
-  match fibonacci number with
-  | Ok result ->
-    Printf.printf "Fibonacci(%d) = %s\n" number (Z.to_string result);
-    `Ok ()
-  | Error msg ->
-    Printf.eprintf "Error: %s\n" msg;
-    `Error (false, msg)
 ;;
 
 let cmd =
--- bin/main.mli
+++ bin/main.mli
@@ -1,6 +1,4 @@
 open Cmdliner
 
-val fibonacci : int -> (Z.t, 'a) result
 val number_arg : int Term.t
-val run : int -> [> `Error of bool * 'a | `Ok of unit]
 val cmd : unit Cmd.t
